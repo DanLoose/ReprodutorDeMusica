43e38331054bd4f10b4b13e401b9f30ee5926aef
Danilo Loose <danloose14@gmail.com>
2021-08-17 21:07:01 -0300

    novas musicas + efeitos visuais

--- JS/dash.js
+++ JS/dash.js
@@ -6,26 +6,38 @@ let buttonRock = document.getElementById("buttonRock");
 let buttonFunk = document.getElementById("buttonFunk");
 let buttonEletronica = document.getElementById("buttonEletronica");
 
-buttonRock.addEventListener("click", selectRock);
-buttonFunk.addEventListener("click", selectFunk);
+// buttonRock.addEventListener("click", selectRock);
+// buttonFunk.addEventListener("click", selectFunk);
 buttonEletronica.addEventListener("click", selectEletronica);
 
 function selectRock(){
     rock.style.display = "block";
     funk.style.display = "none";
     eletronica.style.display = "none";
+
+    buttonRock.style.color = "#1ABC9C";
+    buttonFunk.style.color = "white";
+    buttonEletronica.style.color = "white";
 }
 
 function selectFunk(){
     rock.style.display = "none";
     funk.style.display = "block";
     eletronica.style.display = "none";
+
+    buttonRock.style.color = "white";
+    buttonFunk.style.color = "#1ABC9C";
+    buttonEletronica.style.color = "white";
 }
 
 function selectEletronica(){
     rock.style.display = "none";
     funk.style.display = "none";
-    eletronica.style.display = "block";
+    eletronica.style.display = "block"; 
+
+    buttonRock.style.color = "white";
+    buttonFunk.style.color = "white";
+    buttonEletronica.style.color = "#1ABC9C";
 }
 
 // Music Control 
@@ -36,9 +48,8 @@ function changeMusic(audio){
     playMusic();
 }
 
-// Play & Pause
-
 function playMusic(){
+
     let play = document.getElementById("play").style;
     let pause = document.getElementById("pause").style;
 
@@ -47,7 +58,7 @@ function playMusic(){
         play.display = "none";
         pause.display = "block";
     }
-    
+  
 }
 
 function pauseMusic(){
@@ -59,9 +70,36 @@ function pauseMusic(){
     pause.display = "none";
 }
 
+var repeat = false;
+let repetirAtivado = document.getElementById("repetirAtivado");
+let repetirDesativado = document.getElementById("repetir");
+
+function setRepeat(){
+    repeat = !repeat;
+    
+    repeat == true? repetirAtivado.style.display = "block" : repetirAtivado.style.display = "none";
+    repeat == true? repetirDesativado.style.display = "none" : repetirDesativado.style.display = "block";
+
+    while(repeat == true){
+        audioControl.loop();
+    }
+}
+
+var random = false;
+let randomAtivado = document.getElementById("aleatorio");
+let randomDesativado = document.getElementById("aleatorioAtivado");
+
+function setRandom(){
+    random = !random;
+    
+    random == true? randomAtivado.style.display = "block" : randomAtivado.style.display = "none";
+    random == true? randomDesativado.style.display = "none" : randomDesativado.style.display = "block";
+}
 
 function selectMusic(e){
+
     let audio = e.children[2];
     changeMusic(audio);
+    
 }
 
